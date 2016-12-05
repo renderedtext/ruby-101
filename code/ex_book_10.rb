06037083bb3ca9e9282418673f405ebb5b38f2ea
Filip Dimovski <rexich@riseup.net>
2016-12-05 16:35:49 +0100

    Wrote a topic about command line arguments; added some array methods; fixed the permissions of Ruby scripts with chmod +x, making them executable; wrote example code for working with command line arguments.

---
+++ code/ex_book_10.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+# Working with command line arguments
+
+puts "You provided #{ARGV.size} arguments from the command line."
+print "Those are: "
+p ARGV  # It means: 'ARGV.inspect ; puts'
